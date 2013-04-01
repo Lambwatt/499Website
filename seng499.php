40d21943e89036082988dde42aa07cd5529fbdd2
Beyamor <beyamor@gmail.com>
2013-04-01 15:01:16 -0700

    The start of a pretty awful template system

---
+++ seng499.php
@@ -0,0 +1,3 @@
+<?php include "templates/top.php";?>
+	<p>Here's the page!</p>
+<?php include "templates/bottom.php";?>
---
+++ templates/top.php
@@ -0,0 +1,14 @@
+<!-- The top (everything above the content) of our standard page -->
+<!-- There's undoubtedly a much better way of setting up a php template -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="Stylesheet" href="css/seng499.css" />
+	</head>
+	<body>
+		<div class="nav-bar">
+			<a href="index.html">Home</a> 
+			<a href="report.html">Report</a>
+			<a href="game.html">Demo</a>
+		</div>
+		<div class="content">
