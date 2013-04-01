4f881ddb939fb4e39939efcc575db15a96dfa021
Beyamor <beyamor@gmail.com>
2013-04-01 16:09:08 -0700

    Tweaking game embedding, still doesn't work right

--- game.php
+++ game.php
@@ -1,6 +1,16 @@
 <?php include "templates/top.php";?>
 
-<p>the game</p><br>
-<iframe id="game-frame", width="885", height="665", src="seng499.swf"></iframe>
+<!--
+	For some reason, the Flash content isn't expanding to fill its window on Linux/Chromium.
+	Linux/Firefox seems to work as expected.
+	Right click -> Show All expands the content as desired.
+	Haven't found a fix yet.
+-->
+<object type="application/x-shockwave-flash" data="seng499.swf" width="800" height="600" id="game">
+	<param name="movie" value="seng499.swf"/>
+	<param name="quality" value="high"/>
+	<param name="scale" value="exactfit"/>
+	<embed src="seng499.swf" width="800" height="600" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" scale="exactfit"></embed>
+</object>
 
 <?php include "templates/bottom.php";?>
