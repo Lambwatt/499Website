c17888ab3db95d436fb486be61361c3a32945767
Beyamor <beyamor@gmail.com>
2013-04-01 15:05:10 -0700

    Game page as a php page

---
+++ game.php
@@ -0,0 +1,6 @@
+<?php include "templates/top.php";?>
+
+<p>the game</p><br>
+<iframe id="game-frame", width="885", height="665", src="seng499.swf"></iframe>
+
+<?php include "templates/bottom.php";?>
