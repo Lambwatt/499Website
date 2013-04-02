ba2f4eb662d6a427139a379a1a24641af94a8364
Beyamor <beyamor@gmail.com>
2013-04-01 17:17:52 -0700

    Start of an implementation page

---
+++ implementation.php
@@ -0,0 +1,12 @@
+<?php include "templates/top.php";?>
+
+<!-- Page-specific styling -->
+<link type="text/css" rel="Stylesheet" href="css/implementation.css" />
+
+<!-- Our project -->
+<h1>Source</h1>
+<p>The project's source code is available on <a href="https://github.com/Beyamor/seng499" target="blank">GitHub</a>.</p>
+
+<!-- Things we used -->
+
+<?php include "templates/bottom.php";?>
--- templates/top.php
+++ templates/top.php
@@ -9,9 +9,10 @@
 		<div class="background"></div>
 		<div class="title-bar">Seng 499: Ocean Explorer</div>
 		<div class="nav-bar">
-			<span class="nav-bar-link"><a href="home.php">Home</a></span>
+			<span class="nav-bar-link"><a href="home.php">Project</a></span>
 			<span class="nav-bar-link"><a href="report.php">Report</a></span>
 			<span class="nav-bar-link"><a href="game.php">Game</a></span>
+			<span class="nav-bar-link"><a href="implementation.php">Implementation</a></span>
 		</div>
 		<div class="content-container">
 			<div class="content">
