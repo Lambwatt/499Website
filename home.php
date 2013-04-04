bd97a6ed0050c233873157ffc649642c26f2524c
Beyamor <beyamor@gmail.com>
2013-04-04 15:48:02 -0700

    Started reworking the home page

--- home.php
+++ home.php
@@ -11,16 +11,13 @@
 </span>
 
 <!-- Description -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra, metus sit amet lobortis sodales, augue diam iaculis felis, et pharetra tortor ante in orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vitae ipsum elit. Integer diam purus, fermentum eu accumsan ac, vulputate in arcu. In hac habitasse platea dictumst. Pellentesque eget tempus eros. Quisque malesuada risus quis velit suscipit molestie. Aliquam quis nisi leo, non egestas nisi. Suspendisse faucibus risus ut turpis lobortis nec viverra neque auctor. Aenean feugiat adipiscing ante, a pretium neque lobortis hendrerit. Aenean ut leo nec urna aliquet consequat.</p>
-
-<p>Nulla facilisi. Pellentesque laoreet dictum elit, sit amet pulvinar tortor fermentum sit amet. Nam imperdiet enim eget dui semper eleifend. Ut eget tempus dolor. Sed in neque quam, et varius ipsum. Phasellus viverra nisi in ligula suscipit quis pellentesque lectus venenatis. Praesent nec nisl non risus tristique luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-<p>Nam laoreet, elit vitae ullamcorper egestas, turpis dolor scelerisque turpis, sit amet elementum nibh erat in dui. Quisque at ante quis purus rhoncus molestie. Maecenas sed est cursus lorem pulvinar molestie id at leo. Quisque in felis id lorem rhoncus rhoncus. Etiam ligula velit, volutpat volutpat fermentum nec, laoreet ut nibh. Pellentesque faucibus nisl sed lorem gravida sed tempor dolor aliquet. Suspendisse suscipit nisl eget justo varius tempus. Nulla rhoncus est a sem fringilla pellentesque.</p>
-
-<p>Fusce vel nisl purus. Aliquam eget augue est. Morbi tempor bibendum enim, sit amet ornare dui semper a. Duis laoreet, metus vel bibendum tincidunt, libero ipsum consectetur augue, nec tristique enim tellus quis nisl. Cras dapibus nibh quis lorem iaculis ut blandit diam euismod. Mauris porttitor, tellus quis sodales bibendum, sapien lacus vestibulum dui, at convallis enim quam sed justo. Nulla justo orci, tincidunt sit amet mattis ut, laoreet vel mauris. Aliquam erat volutpat. In arcu tortor, tristique sit amet luctus a, pellentesque eu purus. In volutpat, nunc eu dictum iaculis, nisi lectus viverra mauris, vel consectetur nibh sem ut erat. Integer eget felis a mi lobortis sagittis a sit amet purus. Morbi vitae purus eros. In tincidunt, eros non iaculis hendrerit, mi mauris tempus ligula, at bibendum eros turpis quis purus.</p>
-
-<p>Praesent hendrerit volutpat leo vel rhoncus. Vestibulum ac justo neque, non luctus justo. In hac habitasse platea dictumst. Maecenas id diam sem, sit amet blandit leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean eget augue ut metus auctor dignissim ac eu dolor. Aenean consectetur felis id leo vulputate at pharetra neque varius. Cras sit amet elit sit amet diam molestie gravida id vel neque. Ut molestie nulla sit amet massa scelerisque molestie. Suspendisse potenti.</p>
+<h2>Description</h2>
+<p>We made a thing.</p>
 
 <!-- Team -->
+<h2>Team</h2>
+<p>NEPTUNE: Ocean Explorer was developed by Michael Pattie, Colton Phillips, and Tom Gibson for the engineering design project class at the University of Victoria.</p>
+
+<p>The project was supervised by Doctor Kin Fun Li and sponsored by Maia Hoeberechts.</p>
 
 <?php include "templates/bottom.php";?>
