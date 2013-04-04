33ba4fefe6a11dcca06f8a3736c4e56d46841511
Beyamor <beyamor@gmail.com>
2013-04-04 15:37:18 -0700

    Added resources to the implementation page
    It's pretty ug

--- implementation.php
+++ implementation.php
@@ -8,5 +8,12 @@
 <p>The project's source code is available on <a href="https://github.com/Beyamor/seng499" target="blank">GitHub</a>.</p>
 
 <!-- Things we used -->
+<h1>Resources</h1>
+<h2>FlashPunk</h2>
+NEPTUNE: Ocean Explorer was built on top of the FlashPunk library created by Chevy Ray Johnston.
+<h2>Kyle Pulver</h2>
+A script used to generate embedded asset lists created by Kyle Pulver was modified for use by this project.
+<h2>GnarSound</h2>
+Music was provided by GnarSound for free.
 
 <?php include "templates/bottom.php";?>
