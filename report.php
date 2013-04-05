0d722122f8fb5ca1df6538f167b5f2dfe30aca51
Beyamor <beyamor@gmail.com>
2013-04-05 12:38:33 -0700

    Report link

--- report.php
+++ report.php
@@ -1,5 +1,9 @@
 <?php include "templates/top.php";?>
 
-<p>TODO: Include report</p>
+<link type="text/css" rel="stylesheet" href="css/report.css" />
+
+<a href="ocean-explorer-report.pdf" target="blank" title="Click to download">
+	<img src="images/report.png"/>
+</a>
 
 <?php include "templates/bottom.php";?>
