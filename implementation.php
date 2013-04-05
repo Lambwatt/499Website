cf5f313cf0befa6c99ff63700beb32de6f74e157
Beyamor <beyamor@gmail.com>
2013-04-04 20:32:12 -0700

    Removed "for free"
    From GitHub's editor. Because this is the future.

--- implementation.php
+++ implementation.php
@@ -11,6 +11,6 @@
 <h1>Resources</h1>
 <p><a href="https://twitter.com/ChevyRay" target="blank">FlashPunk</a> - NEPTUNE: Ocean Explorer was built on top of the FlashPunk library created by Chevy Ray Johnston.</p>
 <p><a href="http://kylepulver.com/" target="blank">Kyle Pulver</a> - A script used to generate embedded asset lists created by Kyle Pulver was modified for use by this project.</p>
-<p><a href="http://soundcloud.com/gnarsound" target="blank">GnarSound</a> - Music was provided by GnarSound for free.</p>
+<p><a href="http://soundcloud.com/gnarsound" target="blank">GnarSound</a> - Music was provided by GnarSound.</p>
 
 <?php include "templates/bottom.php";?>
